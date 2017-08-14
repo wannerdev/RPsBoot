17ca4e9572f9b08780dab9f3ccaabf87c8a5759a
wannerdev <johannes.wanner@web.de>
2017-08-14 14:44:56 +0200

    Update bootup.sh

--- bootup.sh
+++ bootup.sh
@@ -6,9 +6,8 @@ sudo echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' > sudo /etc/apt
 sudo apt-get -y update 
 sudo apt-get -y install kubeadm
 sudo apt-get -y install oracle-java8-jdk
-logout
 curl -sS https://get.pimoroni.com/blinkt | bash
-printf 'allow-hotplug eth0\n iface eth0 inet static\n address 192.168.1.42\n netmask 255.255.255.0\n gateway 192.168.1.1\n domain_name_servers=8.8.8.8, 8.8.4.4' > /temp/eth0
+printf 'allow-hotplug eth0\n iface eth0 inet static\n address 192.168.1.42\n netmask 255.255.255.0\n gateway 192.168.1.1\n domain_name_servers=8.8.8.8, 8.8.4.4' > ./eth0
 sudo cp ./eth0 /etc/network/interfaces.d/eth0
 sudo ifdown eth0
 sudo ifup eth0
