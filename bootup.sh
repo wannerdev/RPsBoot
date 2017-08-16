3d809ced92805edb7af309b82d0700bc4d86a020
wannerdev <johannes.wanner@web.de>
2017-08-16 11:00:41 +0200

    Update bootup.sh

--- bootup.sh
+++ bootup.sh
@@ -9,7 +9,26 @@ sudo apt-get -y install kubeadm
 sudo apt-get -y install oracle-java8-jdk
 printf 'allow-hotplug eth0\n iface eth0 inet static\n address 192.168.1.42\n netmask 255.255.255.0\n gateway 192.168.1.1\n domain_name_servers=8.8.8.8, 8.8.4.4' > ./eth0
 sudo cp ./eth0 /etc/network/interfaces.d/eth0
+wget https://bitbucket.org/logicline/kubernetes-hacknight/downloads/blinkled-1.0-all.jar -P /home/pirate/blinkled/
+printf '#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+java -jar /home/pirate/blinkled/blinkled-1.0-all.jar
+
+exit 0' > ./rc.local
+sudo cp ./rc.local /etc/rc.local
 sudo ifdown eth0
 sudo ifup eth0
-sudo rm ./eth0
+rm ./eth0
+rm ./rc.local
+rm ./kubernetes.list
 curl -sS https://get.pimoroni.com/blinkt | bash
